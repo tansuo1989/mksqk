ca5d3a884a6fa40e70f9d9a810ef87c02696bb79
tansuo1989 <huangui9@qq.com>
2018-01-26 10:18:03 +0800

    ..

---
+++ index.js
@@ -0,0 +1,64 @@
+var mysql=require("mysql");
+
+
+function wheresql(obj){
+  var sql="";
+	for(var i in obj){
+		sql+=" "+i+"='"+obj[i]+"' and";
+    }
+    if(!sql){return "";}
+	sql=sql.substring(0,sql.length-4);
+	return " where "+sql;
+}
+
+function filter(sql){
+	return sql.replace("'","\'").replace('"','\"');
+}
+
+exports.filter=filter;
+
+exports.conn=function(config){
+	var connection = mysql.createConnection(config);
+	connection.connect();
+	return connection;	
+}
+
+exports.insert=function(table,obj){
+      var sql="insert into "+table+" ";
+      var key="",value="";
+      for(var i in obj){
+      	key+=filter(i)+",";
+      	value+="'"+filter(obj[i])+"',";
+      }
+      key=key.substring(0,key.length-1);
+      value=value.substring(0,value.length-1);
+      var sql='INSERT INTO '+filter(table)+" ("+key+") VALUES ("+value+")";
+      return sql;
+	}
+
+exports.select=function(table,fields,where){
+   var fields=fields?fields:"*";
+   var where=where?wheresql(obj):"";
+   var sql="select "+filter(fields)+" from "+filter(table)+where;
+   return sql;
+}
+
+exports.where=wheresql;
+
+exports.update=function(table,obj,where){
+	if(!where){return false};
+
+	var where=wheresql(where);
+
+    var sql="update "+filter(table)+" set ";
+	for(var i in obj){
+		sql+=" "+filter(i)+"='"+filter(obj[i])+"' ,";
+	}
+	sql=sql.substring(0,sql.length-1);
+	return sql+where;
+}
+
+exports.delete=function(table,where){
+  if(!where){return false};
+  return "delete from "+filter(table)+wheresql(where);
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+#使用方法
+1. 引入mksql
+``` javascript
+    var mk=require("mksql");
+``` 
+2. 连接数据库：
+``` javascript
+    var config={
+        host     : 'localhost',
+        port     :  3307,
+        user     : 'root',
+        password : '',
+        database : 'test'
+    }
+    //建议配置信息从配置文件读取
+
+    var conn=mk.conn(config);
+```
+3. 生成 sql 语句
+``` javascript
+    var sql=mk.select("tablename","id,name",{id:3});
+
+    var sql=mk.insert("tablename",{name:"lilei",age:20});
+
+    var sql=mk.update("table",data,where);
+
+    var sql=mk.delete("table",where);
+```
+4. 直接使用conn.query 方法：
+``` javascript
+    conn.query(sql,function(err,res){
+        console.log(err,res);
+    })
+```
+# todo:
+ * delete 和 update 的时候，当 where 条件不成立时，考虑报错
+ * 添加 pdo 的对象绑定sql语句的方式
